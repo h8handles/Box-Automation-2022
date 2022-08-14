dd0d7fc2b78e6ce2e5f95e4f636a19b0fe6c7e37
h8handles <h8handles@gmail.com>
2022-08-14 04:16:18 -0700

    integrated bloodhound automation

--- box-start.sh
+++ box-start.sh
@@ -19,29 +19,25 @@ mkdir htb;mkdir tryhackme; mkdir web;
 # create opt structure for tools web for uploads  wordlists
 
 echo -e "\e[92mInstalling Terminal Environment ....\e[0m"
-sudo apt install git xclip terminator -y
+sudo apt install git python3-pip virtualenv xclip terminator -y
 
 # install zsh/oh-my-zsh
 
 echo -e "\e[92mSetting up Bloodhound ....\e[0m"
 
-echo "deb http://httpredir.debian.org/debian stretch-backports main" | sudo tee -a /etc/apt/sources.list.d/stretch-backports.list
+wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
+
+echo 'deb https://debian.neo4j.com/ stable 4.4' | sudo tee -a /etc/apt/sources.list.d/neo4j.list
 
 sudo apt-get update
+sudo apt install neo4j
 
-wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
-echo 'deb https://debian.neo4j.com stable 4.0' > /etc/apt/sources.list.d/neo4j.list
-sudo apt-get install apt-transport-https
-sudo apt-get install neo4j
-sudo systemctl stop neo4j
-cd /usr/bin
-./neo4j console
-cd ~/Downloads
-wget https://github.com/BloodHoundAD/BloodHound/releases/download/4.2.0/BloodHound-linux-arm64.zip
-cp BloodHound-linux-arm64.zip /opt
+cd /opt/
 
-cd /opt
-unzip BloodHound-linux-arm64.zip
+wget https://github.com/BloodHoundAD/BloodHound/releases/download/4.2.0/BloodHound-linux-x64.zip
 
-echo -e "\e[92mMAKE SURE TO CHANGE CREDS FOR NEO4J!!!! ....\e[0m"
+unzip BloodHound-linux-x64.zip
+
+
+echo "\e[92mMAKE SURE TO CHANGE CREDS FOR NEO4J!!!! ....\e[0m"
 
